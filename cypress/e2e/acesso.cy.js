74b2550872c8bb5dcd65ab95f9f5bae0a208ebff
vnathale <vitornathale@gmail.com>
2023-02-08 22:07:58 -0300

    feature: Gerenciamento de Pacotes

--- cypress/e2e/acesso.cy.js
+++ cypress/e2e/acesso.cy.js
@@ -2,7 +2,6 @@ describe('Realizando o Acesso na página do DevFinance com sucesso.', () => {
   it('Deverá verificar se o Sistema está funcionando', () => {
     cy.visit('https://dev-finance.netlify.app/')
     cy.screenshot('Evidências realizada com sucesso')
-    cy.wait(1000);
-            
+   
   })
 })
